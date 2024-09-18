48095317f73df96012c6874613e157b34d5c2807
Gabriel Prata <gabrielsprata@hotmail.com>
2024-09-18 16:03:56 -0300

    [style] reestilizada a tela de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_biblioteca/screens/register_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
@@ -44,20 +45,60 @@ class LoginScreen extends StatelessWidget {
                       controller: _senhaController,
                       decoration: InputDecoration(hintText: 'Senha'),
                     ),
-                    SizedBox(height: 16),
-                    ElevatedButton(onPressed: () {}, child: Text('Entrar')),
-                    SizedBox(height: 16),
+                    SizedBox(height: 30),
                     ElevatedButton(
-                        onPressed: () {}, child: Text('Entrar com Google')), //Trocar pela Logo do Google
+                      onPressed: () {},
+                      child: Text(
+                        'Entrar',
+                        style: GoogleFonts.montserrat(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                      style: const ButtonStyle(
+                        backgroundColor:
+                            WidgetStatePropertyAll<Color>(Colors.blue),
+                        minimumSize: WidgetStatePropertyAll<Size>(
+                            Size(double.infinity, 50)),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                        style: const ButtonStyle(
+                          minimumSize: WidgetStatePropertyAll<Size>(
+                              Size(double.infinity, 50)),
+                          backgroundColor:
+                              WidgetStatePropertyAll<Color>(Colors.white),
+                          side: WidgetStatePropertyAll(
+                            BorderSide(
+                              color: Colors.blue, // Cor da borda
+                              width: 2.0, // Largura da borda
+                            ),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text('Entrar com Google',
+                            style: GoogleFonts.montserrat(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.blue,
+                            ))), //Trocar pela Logo do Google
                     SizedBox(height: 16),
                     TextButton(
                       onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context)=> RegisterScreen()
-                        )
-                        );
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => RegisterScreen()));
                       },
-                      child: Text('Ainda não tem uma conta? Clique aqui para criar'),
+                      child: Text(
+                          'Ainda não tem uma conta? Clique aqui para criar',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.blue.shade700,
+                          )),
                     ),
                   ],
                 ),
